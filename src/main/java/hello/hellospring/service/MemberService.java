25a68d70575646bfcf810fd772e3b14a8ff764f5
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-05 21:05:01 +0900

    2020-09-05, 한주안, 네번째 커밋

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,26 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     *  회원 가입
+     */
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원x
+        // 결과는 optional 멤버니까 이러한 코딩이 가능하다.
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
