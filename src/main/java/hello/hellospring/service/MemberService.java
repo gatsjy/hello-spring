029c99ec5065223a1095c03f00a970d99bb4a6e8
gatsjy <38937414+gatsjy@users.noreply.github.com>
2020-09-05 21:20:16 +0900

    2020-09-05, 한주안, 회원가입, 전체회원조회 관련 서비스

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -16,11 +17,27 @@ public class MemberService {
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원x
         // 결과는 optional 멤버니까 이러한 코딩이 가능하다.
+        validateDuplicateMember(member); // 중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-        memberRepository.save(member);
-        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     * 서비스는 비즈니스 로직에 의존적으로 개발해야 한다.
+     * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
